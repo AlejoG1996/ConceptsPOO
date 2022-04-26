359889c598ef6372a9bcbd4bf8f626076fbb9c7a
AlejoG1996 <89146947+AlejoG1996@users.noreply.github.com>
2022-04-25 22:22:17 -0500

    Finish Class Date

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("=========");
+
+try
+{
+    Console.WriteLine(new Date(2024, 02, 29));
+    Console.WriteLine(new Date(1974, 9, 07));
+    Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception Error)
+{
+    Console.WriteLine(Error.Message);
+}
