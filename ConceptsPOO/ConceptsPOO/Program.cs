ad659046b1fef72f9b4e9f62da151950610b37d0
AlejoG1996 <89146947+AlejoG1996@users.noreply.github.com>
2022-04-26 15:27:14 -0500

    Finish PooConcepts

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -15,7 +15,8 @@ Employee employee1 = new SalaryEmployee()
     
 };
 
-Console.WriteLine(employee1);
+//Console.WriteLine(employee1);
+
 Employee employee2 = new CommissionEmployee()
 {
     Id = 2020,
@@ -28,4 +29,68 @@ Employee employee2 = new CommissionEmployee()
     CommissionPercentaje=0.03F
 };
 
-Console.WriteLine(employee2);
+//Console.WriteLine(employee2);
+
+
+Employee employee3 = new HourlyEmployee()
+{
+    Id = 3030,
+    FirstName = "Patricia",
+    LastName = "Gutirrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    HourValue = 12356.56M,
+    Hours = 123.5F
+};
+
+//Console.WriteLine(employee3);
+
+Employee employee4 = new BaseCommissionEmployee()
+{
+    Id = 3030,
+    FirstName = "Jazmin",
+    LastName = "Salazar",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+   Base=860685.45M,
+   Sales=58000000M,
+   CommissionPercentaje=0.015F
+};
+
+//Console.WriteLine(employee4);
+
+ICollection<Employee> employees = new List<Employee>()
+{
+    employee1,employee2,employee3,employee4
+};
+
+decimal payroll = 0;
+foreach(Employee employee in employees)
+{
+    Console.WriteLine(employee);
+    payroll += employee.GetValueToPay();
+}
+Console.WriteLine("        ========");
+Console.WriteLine($"Total:       {$"{payroll:C2}",15}");
+
+Invoice invoice1 = new Invoice()
+{
+    Description = "iphone 13",
+    Id = 1,
+    Price = 5300000M,
+    Quantity = 6
+};
+
+Invoice invoice2 = new Invoice()
+{
+    Description = "Posta Premium",
+    Id = 2,
+    Price = 32000M,
+    Quantity = 17.5F
+};
+
+Console.WriteLine(invoice1);
+Console.WriteLine(invoice2);
+
